19340d046cfae905e9167f4b92d30b7d05c04dc1
NCapron <nicolascapron403@gmail.com>
2016-10-04 17:07:54 +0200

    fos

--- src/FormationBundle/Form/PromotionType.php
+++ src/FormationBundle/Form/PromotionType.php
@@ -3,8 +3,6 @@
 namespace FormationBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -18,32 +16,16 @@ class PromotionType extends AbstractType
     {
         $builder
             ->add('titre')
-            ->add('file', array('label' => ' ', 'required' => false))
+            ->add('file', 'file', array('label' => ' ', 'required' => false))
             ->add('nomForm')
             ->add('prenomForm')
-            ->add('semaines', ChoiceType::class, array(
-                'choices' => array(
-                    '1' => '1',
-                    '2' => '2'
-                ),
-                'required'    => true,
-                'placeholder' => 'Selectionnez le nombre',
-                'empty_data'  => null
-            ))
+            ->add('semaines')
+            ->add('dateDeb', 'date')
+            ->add('dateFin', 'date')
             ->add('module')
             ->add('langage')
-            ->add('eleve')
             ->add('intervenant')
-            ->add('dateDeb', DateType::class, array(
-                'widget' => 'single_text',
-                'attr' => ['class' => 'datepicker'],
-                'format' => 'dd-MM-yyyy'
-            ))
-            ->add('dateFin', DateType::class, array(
-                'widget' => 'single_text',
-                'attr' => ['class' => 'datepicker'],
-                'format' => 'dd-MM-yyyy'
-            ))
+            ->add('eleve')
         ;
     }
     
