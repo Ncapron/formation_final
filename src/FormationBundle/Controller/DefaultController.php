35e957234c228ee83aae3238ac3244338d07da80
Deimokle <q.dutrevis@gmail.com>
2016-10-06 23:34:40 +0200

    quentin ajout de tableau + boucle module

--- src/FormationBundle/Controller/DefaultController.php
+++ src/FormationBundle/Controller/DefaultController.php
@@ -2,12 +2,22 @@
 
 namespace FormationBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use FormationBundle\Entity\Module;
+use FormationBundle\Form\ModuleType;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('FormationBundle:Default:index.html.twig');
+    	$em = $this->getDoctrine()->getManager();
+
+        $modules = $em->getRepository('FormationBundle:Module')->findAll();
+
+        return $this->render('FormationBundle:Default:index.html.twig', array(
+        	'modules' => $modules,
+        	));
     }
 }
