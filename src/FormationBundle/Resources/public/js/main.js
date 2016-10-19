12d49166b535e2de6efb77cb5e6fae5a3d00a845
NCapron <nicolascapron403@gmail.com>
2016-10-19 11:15:43 +0200

    modal

--- src/FormationBundle/Resources/public/js/main.js
+++ src/FormationBundle/Resources/public/js/main.js
@@ -10,54 +10,16 @@ $ (document).ready(function() {
 });
 
 
-// Get the modal
-var modal = document.getElementById('myModal');
+$(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+});
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+$('#modal1').openModal();
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
-}
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-};
+$('#modal1').closeModal();
 
-// Get the modal
-var modal1 = document.getElementById('myModaleleve');
 
-// Get the button that opens the modal
-var btn1 = document.getElementById("myBtn1");
-
-// Get the <span> element that closes the modal
-var span1 = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn1.onclick = function() {
-    modal1.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span1.onclick = function() {
-    modal1.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal1) {
-        modal1.style.display = "none";
-    }
-};
